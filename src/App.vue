2f0ec9a1111c60637769c667e18d800743db80cb
NorthwesternDirector <fanshundee@126.com>
2019-10-22 15:58:10 +0800

    配置杂项，设置项目文件目录

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    iMUSIC：嗨～不错哦～
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  #app
+    color:$color-theme
 </style>
