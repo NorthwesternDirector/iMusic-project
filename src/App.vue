8258465542fe679299beb0c409bd39234dc80108
NorthwesternDirector <fanshundee@126.com>
2019-10-23 10:03:43 +0800

    设计编写主页面导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <m-tab></m-tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import MHeader from 'components/m-header/m-header'
+import MTab from 'components/m-tab/m-tab'
 
 export default{
   components: {
-    'm-header': MHeader
+    'm-header': MHeader,
+    'm-tab': MTab
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import fastclick from 'fastclick'// 解决移动端点击300ms的延迟问题
+import fastclick from 'fastclick'// 解决移动端点击300ms的延迟问题 第一步
 
 import 'common/stylus/index.styl'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false// 解决移动端点击300ms的延迟问题 第二步
 
 fastclick.attach(document.body)
 
